da5f583bff2277a6bbb4f3e48d31c76644b1a29b
Jorge Sailema <jsailema4049@uta.edu>
2025-10-07 10:32:19 -0500

    FALCON SAILEMA 10:30 Inicio de la prueba

---
+++ examen_software_Falcon_Sailema/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace examen_software_Falcon_Sailema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
